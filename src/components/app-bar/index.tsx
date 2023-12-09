4772ba8ecbb23434c6be58ae4cfdb23bf7caab17
JeJoestar <gekakaraza@gmail.com>
2023-12-09 11:47:00 +0200

    feat: app bar styles

--- src/components/app-bar/index.tsx
+++ src/components/app-bar/index.tsx
@@ -5,6 +5,9 @@ import {
 } from '@mui/material';
 import clsx from 'clsx';
 import { useMemo } from 'react';
+import { RiAccountCircleLine } from "react-icons/ri";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { RiChat4Line } from "react-icons/ri";
 
 interface Props {
 }
@@ -12,9 +15,17 @@ interface Props {
 const AppBar: React.FC<Props> = ({ }) => {
   const content = useMemo(() => {
     return (
-      <>
+      <div className="flex justify-between w-full">
         <Typography variant="h3" className="text-white">Якість життя</Typography>
-      </>
+        <div className="flex gap-4 items-center">
+            <RiChat4Line size={24} className="shrink-0"/>
+            <IoMdNotificationsOutline size={24} className="shrink-0" />
+            <div className="flex flex-col justify-center items-center">
+                <RiAccountCircleLine size={24} className="shrink-0" />
+                <Typography className="text-white" variant="subtitle1">Акаунт</Typography>
+            </div>
+        </div>
+      </div>
     )
   }, []);
 
